a2d8b5236c47b7cba6acd83702a7b89c4deb8e6f
Salinger2333 <1565664814@qq.com>
2025-11-19 19:48:57 +0800

    完成可以在控制台进行的游戏部分

--- script.js
+++ script.js
@@ -1,78 +1,130 @@
-
-
-const Gameboard = (function () {
-    //创建一个二维数组为棋盘
-    let board = []
-    for (let i = 0; i < 3; i++) {
-        board[i] = []
-        for (let j = 0; j < 3; j++)
-            board[i].push(Cell())
-    }
-    const printBoard = () => {
-        const boardWithCellValues = board.map((row) => {
-            return row.map((cell) => {
-                return cell.getValue()
-            })
-        })
-        console.table(boardWithCellValues);
-    }
-
-    const changeBoard = function (rows, cols, mark) {
-        board[rows][cols] = mark
-    }
-
-    const clearBoard = function () {
-
-    }
-
-    return {
-        printBoard,
-        changeBoard
-    }
-})()
-
 function Cell() {
-    let value = 0
+    let value = null
     const getValue = () => value
-
     const addToken = (token) => {
+        if (value !== null) {
+            console.log('这里下过了');
+            return false
+        }
         value = token
+        return true
     }
-
     return {
         getValue,
         addToken
     }
 }
 
-const gameFlow = function () {
-    const placeMark = function () {
+const Player = (name, mark) => {
+    return {
+        name,
+        mark
+    }
+}
+const player1 = Player('player1','x')
+const player2 = Player('player2','o')
 
+const Gameboard = (function () {
+    //创建一个二维数组为棋盘
+    let board = []
+    const init = () => {
+        for (let i = 0; i < 3; i++) {
+            board[i] = []
+            for (let j = 0; j < 3; j++)
+                board[i].push(Cell())
+        }
+    }
+    init()
+
+    const getBoardState = () => {
+        const boardWithCellValues = board.map((row) => {
+            return row.map((cell) => {
+                return cell.getValue()
+            })
+        })
+        return boardWithCellValues
+    }
+    const clearBoard = () => {
+        init()
+    }
+    const dropToken = function (rows, cols, mark) {
+        return board[rows][cols].addToken(mark)
+    }
+
+    const checkWin = function () {
+        const board = getBoardState()
+        for (let i = 0; i < 3; i++) {
+            if (board[i][0] !== null && board[i][0] === board[i][1] && board[i][1] === board[i][2]) {
+                return true
+            }
+        }
+        for (let j = 0; j < 3; j++) {
+            if (board[0][j] !== null && board[0][j] === board[1][j] && board[1][j] === board[2][j]) {
+                return true
+            }
+        }
+        if ((board[0][0] !== null && board[0][0] === board[1][1] && board[1][1] === board[2][2])
+            || (board[2][0] !== null && board[2][0] === board[1][1] && board[1][1] === board[0][2])) {
+            return true
+        }
+        // 没人赢
+        return false
+    }
+    const checkDraw = function () {
+        const board = getBoardState().flat()
+        //含null,则不为平局
+        const isDraw = !board.includes(null)
+        return isDraw
     }
 
 
+    return {
+        getBoardState,
+        dropToken,
+        clearBoard,
+        checkDraw,
+        checkWin
+    }
+})()
 
-    const checkWin = function () {
-        const array = Gameboard.getBoard()
-        // if(array[i][1] === array[i][2]=== array[i][3] )
-        //     array[1][j] ===array[2][j] ===array[2][j] 
-        //     
-        for (let i = 0; i < 3; i++) {
-            for (let j = 0; j < 3; j++) {
+const GameController = (function () {
+    
+    let activatePlayer = player1
+
+    const switchPlayer = () => {
+        return activatePlayer = activatePlayer === player1 ? player2 : player1
+    }
+
+    const getActivatePlayer = () => activatePlayer
+
+    const printNewBoard = () => {
+        console.table(Gameboard.getBoardState())
+    }
+
+
+    // 一回合的完整流程
+    const playRound = function (row, col) {
+        if (Gameboard.dropToken(row, col, getActivatePlayer().mark)) {
+            if (Gameboard.checkWin()) {
+                console.log(`游戏结束,${getActivatePlayer().name}胜利`);
+                printNewBoard()
+                Gameboard.clearBoard()
+
+            } else if (Gameboard.checkDraw()) {
+                console.log(`平局,游戏结束`);
+                Gameboard.clearBoard()
+            } else {
+                switchPlayer()
+                printNewBoard()
+                console.log(`轮到 ${getActivatePlayer().name}的回合:${getActivatePlayer().mark}`);
 
             }
+        } else {
+            console.log('该格已经存在,请重新下');
         }
     }
     return {
-        placeMark,
-        checkWin
+        playRound
     }
-}
+})()
 
-const player = function (name) {
-    const getName = () => name
-
-    return {
-        getName
-    }
-}
